fee6f35705b20c8cfb05e99156da3e45362f5af8
Fred Boivin <fboivin63@gmail.com>
2023-05-25 16:31:04 -0400

    Clean-up some LSP stuff

--- neovim/.config/nvim/lua/config/lsp/null-ls/formatters.lua
+++ neovim/.config/nvim/lua/config/lsp/null-ls/formatters.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 local utils = require "utils"
-local nls_utils = require "config.lsp.null-ls.utils"
 local nls_sources = require "null-ls.sources"
 
 local method = require("null-ls").methods.FORMATTING
@@ -48,17 +47,6 @@ end
 function M.has_formatter(filetype)
   local available = nls_sources.get_available(filetype, method)
   return #available > 0
-end
-
-function M.list_registered(filetype)
-  local registered_providers = nls_utils.list_registered_providers_names(filetype)
-  return registered_providers[method] or {}
-end
-
-function M.list_supported(filetype)
-  local supported_formatters = nls_sources.get_supported(filetype, "formatting")
-  table.sort(supported_formatters)
-  return supported_formatters
 end
 
 return M
