185f991fc18b8b6d2908b20051b1193a5b2704ed
Fred Boivin <fboivin63@gmail.com>
2023-06-15 10:01:36 -0400

    Install Starship differently

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
  if [ -f ~/.zshrc ]; then
   mv ~/.zshrc ~/.zshrc.bak
 fi
+
+if ! command -v starship &> /dev/null
+then
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
 make delete && make
