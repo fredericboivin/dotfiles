bca50025b3fdaa339248d8a14c16eb25aa63b291
Fred Boivin <fboivin63@gmail.com>
2023-08-22 15:40:04 -0400

    Remove null-ls

--- neovim/.config/nvim/lua/config/lsp/init.lua
+++ neovim/.config/nvim/lua/config/lsp/init.lua
@@ -64,7 +64,7 @@ local function on_attach(client, bufnr)
 
   -- Configure highlighting
   require("config.lsp.highlighter").setup(client)
-  require("config.lsp.null-ls.formatters").setup(client, bufnr)
+  -- require("config.lsp.null-ls.formatters").setup(client, bufnr)
 end
 
 local opts = {
@@ -79,7 +79,7 @@ require("config.lsp.handlers").setup()
 
 function M.setup()
   -- null-ls
-  require("config.lsp.null-ls").setup(opts)
+  -- require("config.lsp.null-ls").setup(opts)
   require("config.lsp.installer").setup(servers, opts)
 end
 
