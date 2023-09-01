94aead10f12a73a2f86b0169762c877c65bb24fd
Fred Boivin <fboivin63@gmail.com>
2023-09-01 15:17:12 -0400

    Bigger ToggleTerm

--- neovim/.config/nvim/lua/fredboivin/packer_plugins.lua
+++ neovim/.config/nvim/lua/fredboivin/packer_plugins.lua
@@ -22,6 +22,20 @@ return {
         open_mapping = '<C-g>',
         hide_number = true,
         start_in_insert = true,
+        autochdir = true,
+        shade_terminals = true,
+        float_opts = {
+          width = function()
+            return math.ceil(vim.o.columns * 0.95)
+          end,
+          height = function()
+            return math.ceil(vim.o.lines * 0.90)
+          end,
+          highlights = {
+            border = "FloatBorder",
+            background = "NormalFloat",
+          },
+        },
         direction = 'float', -- vertical | float | tab
       })
     end
