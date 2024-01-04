257edd85710ee65ec775c60b2d5d3677b262f853
Fred Boivin <fboivin63@gmail.com>
2024-01-04 18:43:20 -0500

    Remove deadcode

--- neovim/.config/nvim/lua/fredboivin/lsp_signature.lua
+++
@@ -1,9 +0,0 @@
---require"lsp_signature".setup(
---    {
---        bind = true,
---        handler_opts = {border = "rounded"},
---        verbose = false,
---        floating_window = true,
---        hint_enable = false,
---    }
---)
