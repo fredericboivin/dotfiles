497917daf0ff524e3d20b08f2b7bd4bb025c25cb
Fred Boivin <fboivin63@gmail.com>
2024-01-10 10:31:46 -0500

    Set up stable nvim during install

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@ if [ -f ~/.zshrc ]; then
 	mv ~/.zshrc ~/.zshrc.bak
 fi
 
-# Install the antigen plugin/theme manager if it's not already installed.
 if [[ ! -d $HOME/antigen ]]; then
-	echo -e "Antigen not found, installing..."
 	cd $HOME
 	git clone https://github.com/zsh-users/antigen.git
 	cd -
@@ -22,5 +20,8 @@ if ! command -v bob &> /dev/null
 then
 	cargo install bob-nvim@2.5.0 --locked
 fi
+
+bob install stable
+bob use stable
 
 make delete && make
