987e7d6d93122c53fdb702403d39230de7a94f7f
Fred Boivin <fboivin63@gmail.com>
2024-01-30 22:18:39 -0500

    Enable incremental selection

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -12,21 +12,19 @@ function M.setup()
     sync_install = false,
 
     highlight = {
-      -- `false` will disable the whole extension
       enable = true,
       additional_vim_regex_highlighting = false,
-      use_languagetree = true
     },
 
-    -- incremental_selection = {
-    --   enable = true,
-    --   keymaps = {
-    --     init_selection = "gnn",
-    --     node_incremental = "grn",
-    --     scope_incremental = "grc",
-    --     node_decremental = "grm",
-    --   },
-    -- },
+     incremental_selection = {
+       enable = true,
+       keymaps = {
+         init_selection = "gnn",
+         node_incremental = "grn",
+         scope_incremental = "grc",
+         node_decremental = "grm",
+       },
+     },
 
     indent = { enable = true },
 
