9bc091d1493aec494be40cf9213d464dd1be9c23
Fred Boivin <fboivin63@gmail.com>
2024-05-24 13:35:28 -0400

    Remove slanted bufferline

--- neovim/.config/nvim/lua/config/bufferline.lua
+++ neovim/.config/nvim/lua/config/bufferline.lua
@@ -4,7 +4,6 @@ function M.setup()
   require("bufferline").setup({
     options = {
       diagnostics = "nvim_lsp",
-      separator_style = "slant",
       diagnostics_indicator = function(count, level)
         local icon = level:match("error") and " " or " "
         return " " .. icon .. count
