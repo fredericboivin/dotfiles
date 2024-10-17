bea79ee7191b85630e0be8f8eba5c2102c9772b9
Fred Boivin <fboivin63@gmail.com>
2024-10-17 11:17:06 -0400

    Bump

--- neovim/.config/nvim/lua/config/lsp/installer.lua
+++ neovim/.config/nvim/lua/config/lsp/installer.lua
@@ -18,22 +18,22 @@ function M.setup(servers, server_options)
       local opts = vim.tbl_deep_extend("force", server_options, servers[server_name] or {})
       lspconfig[server_name].setup(opts)
     end,
-    ["ruby_ls"] = function()
-      local opts = vim.tbl_deep_extend("force", server_options, servers["ruby_ls"] or {})
-      require 'lspconfig'.ruby_ls.setup(opts)
-    end,
+    -- ["ruby_ls"] = function()
+    --   local opts = vim.tbl_deep_extend("force", server_options, servers["ruby_ls"] or {})
+    --   require 'lspconfig'.ruby_ls.setup(opts)
+    -- end,
     ["sorbet"] = function()
       local opts = vim.tbl_deep_extend("force", server_options, servers["sorbet"] or {})
       require 'lspconfig'.sorbet.setup(opts)
     end,
-    ["tsserver"] = function()
-      local opts = vim.tbl_deep_extend("force", server_options, servers["tsserver"] or {})
-      require("typescript").setup {
-        disable_commands = false,
-        debug = true,
-        server = opts,
-      }
-    end,
+    -- ["ts_ls"] = function()
+    --   local opts = vim.tbl_deep_extend("force", server_options, servers["ts_ls"] or {})
+    --   require("typescript").setup {
+    --     disable_commands = false,
+    --     debug = true,
+    --     server = opts,
+    --   }
+    -- end,
   }
 end
 
